300eb511d0233ab4c4f380d220c3e290501dfa5f
digiwaresupport <52020718+digiwaresupport@users.noreply.github.com>
2019-06-21 10:04:34 +0700

    Update Smarthome_with_RPi_and_Telegram_Bot.py

--- Raspberry-Pi/Smarthome_with_RPi_and_Telegram_Bot.py
+++ Raspberry-Pi/Smarthome_with_RPi_and_Telegram_Bot.py
@@ -16,8 +16,9 @@ def off(pin):
 GPIO.setmode(GPIO.BOARD)
 
 # Pengaturan GPIO 
-GPIO.setup(15, GPIO.OUT)
-GPIO.setup(16, GPIO.OUT)
+GPIO.setup(37, GPIO.OUT)
+GPIO.setup(38, GPIO.OUT)
+GPIO.setup(40, GPIO.OUT)
 
 def handle(msg):
     chat_id = msg['chat']['id']
@@ -28,32 +29,26 @@ def handle(msg):
     # Menyalakan channel relay 1
     if command == 'nyala1':
        bot.sendMessage(chat_id, on(37))
-       print 'CHANNEL 1 AKTIF'
        
     # Menyalakan channel relay 2
     elif command == 'nyala2':
        bot.sendMessage(chat_id, on(38))
-       print 'CHANNEL 2 AKTIF'
        
     # Menyalakan channel relay 3
     elif command == 'nyala3':
        bot.sendMessage(chat_id, on(40))
-       print 'CHANNEL 3 AKTIF'
     
     # Memadamkan channel relay 1
     elif command =='padam1':
        bot.sendMessage(chat_id, off(37))
-       print 'CHANNEL 1 PADAM'
 
     # Memadamkan channel relay 2
     elif command =='padam2':
        bot.sendMessage(chat_id, off(38))
-       print 'CHANNEL 2 PADAM'
        
     # Memadamkan channel relay 3
     elif command =='padam3':
        bot.sendMessage(chat_id, off(40))
-        print 'CHANNEL 3 PADAM'
        
 bot = telepot.Bot('Bot Token') # Ganti 'Bot Token' dengan kode token anda, misal bot = telepot.Bot('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9')
 bot.message_loop(handle)
